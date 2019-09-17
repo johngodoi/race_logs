93210ee5b111e446cb413dea9096894b1f69f40b
John Henrique Teixeira de Godoi <jgodoi1@its.jnj.com>
2019-09-17 10:12:20 -0300

    Clean-up

--- src/main/scala/com/johngodoi/gympass/race/LogRecord.scala
+++ src/main/scala/com/johngodoi/gympass/race/LogRecord.scala
@@ -2,4 +2,4 @@ package com.johngodoi.gympass.race
 
 import java.time.{Duration, LocalTime}
 
-case class LogRecord(time: LocalTime, codigoPiloto: String, piloto: String, nroVolta: Int, tempoVolta: Duration, velocidadeMediaVolta: Double)
+case class LogRecord(time: LocalTime, code: String, name: String, lapNumber: Int, lapTime: Duration, lapAverageSpeed: Double)
--- src/main/scala/com/johngodoi/gympass/race/RaceMetrics.scala
+++ src/main/scala/com/johngodoi/gympass/race/RaceMetrics.scala
@@ -4,41 +4,40 @@ import java.time.Duration
 
 object RaceMetrics {
 
+  private def groupByPilot(records: List[LogRecord]) = records.groupBy(_.name)
+
   def rankWithTimeDifferences(records: List[LogRecord]):List[(String,Duration)] = {
-    val finishingTime = records
-      .groupBy(_.piloto).map(pr => (pr._1, pr._2.map(_.time).max))
+    val finishingTime = groupByPilot(records).map(pr => (pr._1, pr._2.map(_.time).max))
     val smallerFinishingTime = finishingTime.values.min
     finishingTime.map(ft =>(ft._1,Duration.between(ft._2,smallerFinishingTime)))
       .toList.sortWith((r1,r2) => r1._2.compareTo(r2._2)>0)
   }
 
-  def averageSpeedByPilot(records: List[LogRecord]):List[(String,Double)] = records
-    .groupBy(_.piloto)
+  def averageSpeedByPilot(records: List[LogRecord]):List[(String,Double)] = groupByPilot(records)
     .map(pr => (
       pr._1,
-      pr._2.map(_.velocidadeMediaVolta).sum/pr._2.size
+      pr._2.map(_.lapAverageSpeed).sum/pr._2.size
     )).toList
 
-  def bestLap(records: List[LogRecord]):(String,Duration) = records
-    .groupBy(_.piloto)
-    .map(r => (r._1,r._2.map(_.tempoVolta).min))
-    .minBy(_._2)
+  def bestLap(records: List[LogRecord]):(String,Duration) = groupByPilot(records)
+    .map(r => (
+      r._1,
+      r._2.map(_.lapTime).min
+    )).minBy(_._2)
 
-  def bestLapsByPilot(records: List[LogRecord]):List[(String, Duration)] = records
-    .groupBy(_.piloto)
+  def bestLapsByPilot(records: List[LogRecord]):List[(String, Duration)] = groupByPilot(records)
     .map(pr => (
       pr._1,
-      pr._2.map(_.tempoVolta).min
+      pr._2.map(_.lapTime).min
     )).toList
 
   def rank(records: List[LogRecord]):List[(Int, String, String, Int, Duration)] = {
-    val rank = records
-      .groupBy(_.piloto)
+    val rank = groupByPilot(records)
       .map(pr => (
-        pr._2.map(_.codigoPiloto).head,
+        pr._2.map(_.code).head,
         pr._1,
-        pr._2.map(_.nroVolta).max,
-        pr._2.map(_.tempoVolta).reduce(_ plus _)
+        pr._2.map(_.lapNumber).max,
+        pr._2.map(_.lapTime).reduce(_ plus _)
       )).toList.sortWith((pr1, pr2) => pr1._4.compareTo(pr2._4) < 0)
     (for (p <- rank.indices) yield (p+1, rank(p)._1, rank(p)._2, rank(p)._3, rank(p)._4)).toList
   }
